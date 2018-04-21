c4fa157f6aded3dc5dc9bd862a5c9c2d8a513fa0
Nathan Nichols <n545n545@ku.edu>
2018-04-21 18:40:43 -0500

    Made a function

--- scrapeperry.py
+++ scrapeperry.py
@@ -3,15 +3,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-def milfordtemp()
+def perrytemp():
 	url = "http://www.nwk.usace.army.mil/Locations/District-Lakes/Perry-Lake/Daily-Lake-Info-2/"
 
 	r = requests.get(url)
 
 	soup = BeautifulSoup(r.text, "html.parser")
-
+	
+	temp = 0
+	
 	for i in soup.find_all('p'):
 		if 'Water Surface Temperature:' in i.contents[0]:
-			return(int(str(i.getText()[28])+str(i.getText()[29])))
+			return (int(str(i.getText()[28])+str(i.getText()[29])))
 
-    
+
+
