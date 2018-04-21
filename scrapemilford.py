a755279a50605a749146833a4bfa2e799d43a38f
Nathan Nichols <n545n545@ku.edu>
2018-04-21 18:42:51 -0500

    made into a function

--- scrapemilford.py
+++ scrapemilford.py
@@ -3,13 +3,16 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "http://www.nwk.usace.army.mil/Locations/District-Lakes/Milford-Lake/Daily-Lake-Info-2/"
+def milfordtemp():
+	url = "http://www.nwk.usace.army.mil/Locations/District-Lakes/Milford-Lake/Daily-Lake-Info-2/"
 
-r = requests.get(url)
+	r = requests.get(url)
 
-soup = BeautifulSoup(r.text, "html.parser")
+	soup = BeautifulSoup(r.text, "html.parser")
 
-for i in soup.find_all('span'):
-	if i.getText() == 'Lake Water Surface Temperature:':
-		print (int(i.find_next().getText().strip()))
+	for i in soup.find_all('span'):
+		if i.getText() == 'Lake Water Surface Temperature:':
+			return (int(i.find_next().getText().strip()))
+
+
 
