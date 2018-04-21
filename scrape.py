6305c1337b512e9f7cc7d0f13ffbd4f55241e7c1
Andre Kurait <andrekurait@ku.edu>
2018-04-21 13:46:55 -0700

    Basic Implementation of BeautifulSoup added with scraping lake titles

--- scrape.py
+++ scrape.py
@@ -8,8 +8,11 @@ url = "http://ksoutdoors.com/Fishing/Fishing-Reports/Northeast-Region"
 r = requests.get(url)
 
 soup = BeautifulSoup(r.text, "html.parser")
-
-bigassmfstring = soup.get_text()
-
-print (bigassmfstring)
-
+count = 1
+for i in soup.find_all('a'):
+    if '/Fishing/Where-to-Fish-in-Kansas/Fishing-Locations-Public-Waters/Northeast-Region/' in i.attrs['href'].strip():
+        print ('Found ' + str(count)
+         + ':')
+        count = count + 1
+        print (i.contents[0])
+        print('')
