50d8d726a50ae6c5b2651169982d3fb028f9edef
czfloyd <floyd.c23@gmail.com>
2018-04-22 07:28:15 -0500

    Implemented changes for FishAtLake

--- lambda_function.py
+++ lambda_function.py
@@ -264,48 +264,46 @@ def fishatlake(fishtype, lake):
 	count = int(0)
 	for i in soup.find_all('a'):
 	    if '/Fishing/Where-to-Fish-in-Kansas/Fishing-Locations-Public-Waters/Northeast-Region/' in i.attrs['href'].strip():
-		if lake == 'clinton':
-			if 'CLINTON RESERVOIR' in i.contents[0]:
-			    infoTable = i.find_next('table')
-			    for info in infoTable.find_all('tr'):
-				index = int(0)
-				for specificInfo in info.find_all('td'):
-					if fishtype in specificInfo.getText().strip().lower():
-						if specificInfo.find_next().getText().strip() =='Slow' or specificInfo.find_next().getText().strip() == 'Fair' or specificInfo.find_next().getText().strip() =='Good' or specificInfo.find_next().getText().strip() =='Poor' or specificInfo.find_next().getText().strip() =='Excellent':
-							D = [specificInfo.find_next().getText().strip(), specificInfo.getText().strip()]
-							return(D)
-		if lake == 'perry':
-			if 'PERRY RESERVOIR' in i.contents[0]:
-				infoTable = i.find_next('table')
-				for info in infoTable.find_all('tr'):
-					index = int(0)
-					if fishtype == 'bass':
-						for specificInfo in info.find_all('a'):
-							if fishtype in specificInfo.getText().strip().lower():
-								if specificInfo.find_next().getText().strip() =='Slow' or specificInfo.find_next().getText().strip() == 'Fair' or specificInfo.find_next().getText().strip() =='Good' or specificInfo.find_next().getText().strip() =='Poor' or specificInfo.find_next().getText().strip() =='Excellent':
-									D = [specificInfo.find_next().getText().strip(), specificInfo.getText().strip()]
-									return(D)
-					else:
-						for specificInfo in info.find_all('a'):
-							if fishtype in specificInfo.getText().strip().lower():
-								if specificInfo.find_next().getText().strip() =='Slow' or specificInfo.find_next().getText().strip() == 'Fair' or specificInfo.find_next().getText().strip() =='Good' or specificInfo.find_next().getText().strip() =='Poor' or specificInfo.find_next().getText().strip() =='Excellent':
-									D = [specificInfo.find_next().getText().strip(), specificInfo.getText().strip()]
-									return(D)
-
-		if lake == 'milford':
-			if 'MILFORD RESERVOIR' in i.contents[0]:
-				infoTable = i.find_next('table')
-				for info in infoTable.find_all('tr'):
+			if lake == 'clinton':
+				if 'CLINTON RESERVOIR' in i.contents[0]:
+				    infoTable = i.find_next('table')
+				    for info in infoTable.find_all('tr'):
 					index = int(0)
 					for specificInfo in info.find_all('td'):
 						if fishtype in specificInfo.getText().strip().lower():
 							if specificInfo.find_next().getText().strip() =='Slow' or specificInfo.find_next().getText().strip() == 'Fair' or specificInfo.find_next().getText().strip() =='Good' or specificInfo.find_next().getText().strip() =='Poor' or specificInfo.find_next().getText().strip() =='Excellent':
 								D = [specificInfo.find_next().getText().strip(), specificInfo.getText().strip()]
 								return(D)
+			if lake == 'perry':
+				if 'PERRY RESERVOIR' in i.contents[0]:
+					infoTable = i.find_next('table')
+					for info in infoTable.find_all('tr'):
+						index = int(0)
+						if fishtype == 'bass':
+							for specificInfo in info.find_all('a'):
+								if fishtype in specificInfo.getText().strip().lower():
+									if specificInfo.find_next().getText().strip() =='Slow' or specificInfo.find_next().getText().strip() == 'Fair' or specificInfo.find_next().getText().strip() =='Good' or specificInfo.find_next().getText().strip() =='Poor' or specificInfo.find_next().getText().strip() =='Excellent':
+										D = [specificInfo.find_next().getText().strip(), specificInfo.getText().strip()]
+										return(D)
+						else:
+							for specificInfo in info.find_all('a'):
+								if fishtype in specificInfo.getText().strip().lower():
+									if specificInfo.find_next().getText().strip() =='Slow' or specificInfo.find_next().getText().strip() == 'Fair' or specificInfo.find_next().getText().strip() =='Good' or specificInfo.find_next().getText().strip() =='Poor' or specificInfo.find_next().getText().strip() =='Excellent':
+										D = [specificInfo.find_next().getText().strip(), specificInfo.getText().strip()]
+										return(D)
+			if lake == 'milford':
+				if 'MILFORD RESERVOIR' in i.contents[0]:
+					infoTable = i.find_next('table')
+					for info in infoTable.find_all('tr'):
+						index = int(0)
+						for specificInfo in info.find_all('td'):
+							if fishtype in specificInfo.getText().strip().lower():
+								if specificInfo.find_next().getText().strip() =='Slow' or specificInfo.find_next().getText().strip() == 'Fair' or specificInfo.find_next().getText().strip() =='Good' or specificInfo.find_next().getText().strip() =='Poor' or specificInfo.find_next().getText().strip() =='Excellent':
+									D = [specificInfo.find_next().getText().strip(), specificInfo.getText().strip()]
+									return(D)
 
 
 def fishscrape(lake):
-
     url = "http://ksoutdoors.com/Fishing/Fishing-Reports/Northeast-Region"
 
     r = requests.get(url)
@@ -343,6 +341,68 @@ def fishscrape(lake):
                             fish[count][index] = specificInfo.getText().strip()
                             index = index + 1
                         count = count + 1
+            if 'perry'.lower() in lake.lower():
+                if 'perry'.lower() in i.contents[0].lower():
+                    infoTable = i.find_next('table')
+                    for info in infoTable.find_all('tr'):
+                        if skip < 2:
+                            skip = skip + 1
+                            continue
+                        index = int(0)
+                        if info.getText().strip() == '':
+                            continue
+                        for specificInfo in info.find_all('td'):
+                            if 'Water level' in specificInfo.getText().strip():
+                                count = count - 1
+                                break
+                            if 'Newsletter' in specificInfo.getText().strip():
+                                count = count - 1
+                                break
+                            if specificInfo.getText().strip() == '':
+                                if index == 0:
+                                    count = count - 1
+                                    break
+                                fish[count][index] = 'Unknown'
+                                index = index + 1
+                                continue
+                            fish[count][index] = specificInfo.getText().strip()
+                            if 'Largemouth BassSmallmouth Bass' in specificInfo.getText().strip():
+                                fish[count][index] = 'Largemouth Bass and Smallmouth Bass'
+                            if 'FairFair' in specificInfo.getText().strip():
+                                fish[count][index] = 'Fair'
+                            if 'up to 5.0 lbs.up to 3.0 lbs.' in specificInfo.getText().strip():
+                                fish[count][index] = 'up to 5.0 lbs and 3.0 lbs respectively.'
+                            index = index + 1
+                        count = count + 1
+                    count = count - 1
+            if 'milford'.lower() in lake.lower():
+                if 'milford'.lower() in i.contents[0].lower():
+                    infoTable = i.find_next('table')
+                    for info in infoTable.find_all('tr'):
+                        if skip < 1:
+                            skip = skip + 1
+                            continue
+                        index = int(0)
+                        if info.getText().strip() == '':
+                            continue
+                        for specificInfo in info.find_all('td'):
+                            if 'Water level' in specificInfo.getText().strip():
+                                count = count - 1
+                                break
+                            if 'Newsletter' in specificInfo.getText().strip():
+                                count = count - 1
+                                break
+                            if specificInfo.getText().strip() == '':
+                                if index == 0:
+                                    count = count - 1
+                                    break
+                                fish[count][index] = 'Unknown'
+                                index = index + 1
+                                continue
+                            fish[count][index] = specificInfo.getText().strip()
+                            index = index + 1
+                        count = count + 1
+                    count = count - 1
     returnfish = [['' for y in range(3)] for x in range(count-1)]
     for x in range(count-1):
         for y in range(3):
